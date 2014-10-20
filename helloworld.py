1cf8625b032f7a5b91b783ebb7840253b7f58569
Dylan Jones <dyl@anjon.es>
2014-10-20 19:49:08 +0100

    adds a question

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,7 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
+        self.response.write('How are you?')
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
