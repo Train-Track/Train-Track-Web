235f864e7ecbb2ca1969ed4b26c23404087da1a0
Dylan Jones <dyl@anjon.es>
2015-02-23 21:31:10 +0000

    adds customisable user views from devise
    the sign up page has been bootstraped

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :username, length: { in: 2..30 }, uniqueness: true, format: { with: /\A\w+\z/, message: "only letters, numbers and underscores" }
+  validates :username, presence: true, uniqueness: true, format: { with: /\A\w+\z/, message: "only letters, numbers and underscores" }
   validates :email, uniqueness: true
+
+  def image_url(size = 30)
+    return "https://secure.gravatar.com/avatar/#{Digest::MD5::hexdigest(email.strip.downcase)}.jpg?s=#{size}&d=identicon"
+  end
+
+  def to_s
+    username
+  end
+
+  def url
+     "/users/" + username
+  end
 
 end
