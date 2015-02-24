b30a5c801c07e9ba9385e80873f97d14fcb4dda0
Dylan Jones <dyl@anjon.es>
2015-02-24 23:30:48 +0000

    views for a service and needed helper methods

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service
-  attr_reader :id, :service_type, :operator, :station, :sta, :ata, :std, :atd, :previous_calling_points, :subsequent_calling_points
-  attr_writer :id, :service_type, :operator, :station, :sta, :ata, :std, :atd, :previous_calling_points, :subsequent_calling_points
+  attr_reader :id, :service_type, :operator, :station, :sta, :eta, :ata, :std, :etd, :atd, :previous_calling_points, :subsequent_calling_points
+  attr_writer :id, :service_type, :operator, :station, :sta, :eta, :ata, :std, :etd, :atd, :previous_calling_points, :subsequent_calling_points
 
 
   def initialize service_id
@@ -8,10 +8,36 @@ class Service
   end
 
 
+  def to_s
+    return origin.to_s + " to " + destination.to_s
+  end
+
+
+  def calling_points
+    here = CallingPoint.new nil
+    here.station = self.station
+    here.st = self.sta || self.std
+    here.et = self.eta || self.etd
+    here.at = self.ata || self.atd
+    return [previous_calling_points, [here], subsequent_calling_points].flatten
+  end
+
+
+  def origin
+    return calling_points.first
+  end
+
+
+  def destination
+    return calling_points.last
+  end
+
+
   def parse xml
     self.previous_calling_points = []
     self.subsequent_calling_points = []
     self.service_type = xml.css('serviceType').text
+    self.station = Station.find_by crs: xml.css('crs').first.text
     self.operator = Operator.find_by code: xml.css('operatorCode').text
     self.sta = xml.css('sta').text
     self.ata = xml.css('ata').text
@@ -39,11 +65,18 @@ class Service
     attr_reader :station, :st, :et, :at
     attr_writer :station, :st, :et, :at
 
+
     def initialize xml
+      return if xml.nil?
       self.station = Station.find_by crs: xml.css('crs').text
       self.st = xml.css('st').text
       self.et = xml.css('et').text
       self.at = xml.css('at').text
+    end
+
+
+    def to_s
+      station.to_s
     end
 
   end
