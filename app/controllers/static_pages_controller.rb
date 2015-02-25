ae46dd06e0c8ba1d56542768c2a6880120d1b86f
Dylan Jones <dyl@anjon.es>
2015-02-25 01:08:43 +0000

    adds static placeholder pages for legal bits

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
 
+
   # GET /
   # GET /.json
   # GET /.xml
@@ -13,4 +14,45 @@ class StaticPagesController < ApplicationController
     end
   end
 
+
+  # GET /help
+  # GET /help.json
+  # GET /help.xml
+  def help
+    @help = "Coming soon!"
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @help }
+      format.json { render json: @help, callback: params['callback'] }
+    end
+  end
+
+
+  # GET /terms
+  # GET /terms.json
+  # GET /terms.xml
+  def terms
+    @terms = "Coming soon!"
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @terms }
+      format.json { render json: @terms, callback: params['callback'] }
+    end
+  end
+
+
+  # GET /privacy
+  # GET /privacy.json
+  # GET /privacy.xml
+  def privacy
+    @privacy = "Coming soon!"
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @privacy }
+      format.json { render json: @privacy, callback: params['callback'] }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,28 @@
 Rails.application.routes.draw do
+
   root to: 'static_pages#index'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/terms', to: 'static_pages#terms'
+  get '/help', to: 'static_pages#help'
+
+  get '/stations', to: 'stations#index'
   get '/stations', to: 'stations#index'
   get '/stations/:crs', to: 'stations#show'
   get '/stations/:crs/departures', to: 'stations#departures'
   get '/stations/:crs/arrivals', to: 'stations#arrivals'
+
   get '/services/:service_id', to: 'services#show'
+
   get '/operators', to: 'operators#index'
   get '/operators/:code', to: 'operators#show'
+
   devise_for :users, path: "", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'users', sign_up: 'new' }
   get '/users/:username', to: 'users#show'
+
   resources :badges, only: [:index, :show]
+
   resources :journeys do
     resources :journey_legs, path: 'legs', except: [:index]
   end
+
 end
