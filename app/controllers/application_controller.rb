08829e434d217ae5ebdbb7754a92d78b1e111d4b
Dylan Jones <dyl@anjon.es>
2015-03-01 01:22:35 +0000

    adds handler to redirect www. fixes #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :redirect_subdomain
 
   protected
 
@@ -12,4 +13,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
 
+  def redirect_subdomain
+    if request.host == 'www.traintrackapp.co.uk'
+      redirect_to 'http://traintrackapp.co.uk' + request.fullpath, status: :moved_permanently
+    end
+  end
 end
