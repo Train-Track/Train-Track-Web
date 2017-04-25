0555bf03b97c35e77eaa4958dcf42878bc3f8f7c
Dylan Jones <dyl@anjon.es>
2017-04-25 21:57:54 +0100

    #22 add underground api metrics

--- app/models/api_call.rb
+++ app/models/api_call.rb
@@ -1,10 +1,12 @@
+include ActionView::Helpers::TextHelper
 class ApiCall < ActiveRecord::Base
   default_scope { order('created_at DESC') }
 
   def self.add source, url, response, time
+    body = truncate(response.body, length: 1000, escape: false)
     ApiCall.create(
       response_code: response.code,
-      response: response.body,
+      response: body,
       source: source,
       url: url,
       response_time: time
