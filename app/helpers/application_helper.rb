f7f012acd62b7cf2668958ed5ccb477b1d12e565
Dylan Jones <dyl@anjon.es>
2019-12-30 21:15:02 +0000

    #1 Update pagination gem to work with bootstrap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,9 @@ module ApplicationHelper
     return "7912*ASIN(SQRT(POWER(SIN((lat-#{lat})*pi()/180/2),2)+COS(lat*pi()/180)*COS(#{lat}*pi()/180)*POWER(SIN((lng-#{lng})*pi()/180/2),2)))"
   end
 
-  #bootsrap 3 paginator
+  #bootstrap 4 paginator
   def paginate page
-    will_paginate page, renderer: BootstrapPagination::Rails, bootstrap: 3
+    will_paginate page, renderer: WillPaginate::ActionView::BootstrapLinkRenderer
   end
 
   #glyphicons
